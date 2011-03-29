7d7639329d0172b705c79f1f3fd3ab8d2f841da1
John Bragg <remotezygote@gmail.com>
2011-03-28 17:18:55 -0700

    reformat

--- creditcard.js
+++ creditcard.js
@@ -37,58 +37,58 @@
 // Made prototype-free by John Bragg @ Seabright Studios
 
 var CreditCard = {
-  CARDS: {
-    Visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
-    MasterCard: /^5[1-5][0-9]{14}$/,
-    DinersClub: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
-    Amex: /^3[47][0-9]{13}$/,
-    Discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/
-  },
-  TEST_NUMBERS: ('378282246310005 371449635398431 378734493671000 '+
-    '30569309025904 38520000023237 6011111111111117 '+
-    '6011000990139424 5555555555554444 5105105105105100 '+
-    '4111111111111111 4012888888881881 4222222222222'
-  ).split(" "),
-  validate: function(number){
-    return CreditCard.verifyLuhn10(number) && !!CreditCard.type(number) && !CreditCard.isTestNumber(number);
-  },
-  verifyLuhn10: function(number){
+	CARDS: {
+		Visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+		MasterCard: /^5[1-5][0-9]{14}$/,
+		DinersClub: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+		Amex: /^3[47][0-9]{13}$/,
+		Discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/
+	},
+	TEST_NUMBERS: ('378282246310005 371449635398431 378734493671000 '+
+		'30569309025904 38520000023237 6011111111111117 '+
+		'6011000990139424 5555555555554444 5105105105105100 '+
+		'4111111111111111 4012888888881881 4222222222222'
+	).split(" "),
+	validate: function(number){
+		return CreditCard.verifyLuhn10(number) && !!CreditCard.type(number) && !CreditCard.isTestNumber(number);
+	},
+	verifyLuhn10: function(number){
 		var cc = CreditCard.strip(number), sum = 0, end = cc.length-1;
 		for (i = end; i > -1; i--) {
-	    var c = parseInt(cc.charAt(i), 10);
-	    if ((end - i) % 2 != 0) c *= 2;
+			var c = parseInt(cc.charAt(i), 10);
+			if ((end - i) % 2 != 0) c *= 2;
 			c = c.toString();
 			for(var l=0;l<c.length;l++) {
 				sum += parseInt(c[l],10);
 			};
-	  };
+		};
 		return(sum % 10 == 0);
-  },
-  isTestNumber: function(number){
-    return(CreditCard.TEST_NUMBERS.indexOf(CreditCard.strip(number))!=-1);
-  },
-  strip: function(number) {
-    return number.replace(/\s+/g,"");
-  },
-  type: function(number) {
-    for(var card in CreditCard.CARDS) {
-      if(CreditCard['is'+card](number)) return card;
+	},
+	isTestNumber: function(number){
+		return(CreditCard.TEST_NUMBERS.indexOf(CreditCard.strip(number))!=-1);
+	},
+	strip: function(number) {
+		return number.replace(/\s+/g,"");
+	},
+	type: function(number) {
+		for(var card in CreditCard.CARDS) {
+			if(CreditCard['is'+card](number)) return card;
 		};
 		return(undefined);
-  },
+	},
 	bindType: function(card) {
-	  var _function = CreditCard.checkType;
-	  return function() {
-	    return _function.apply(card, arguments);
-	  };
+		var _function = CreditCard.checkType;
+		return function() {
+			return _function.apply(card, arguments);
+		};
 	},
 	checkType: function(number) {
-  	return CreditCard.CARDS[this].test(CreditCard.strip(number));
+		return CreditCard.CARDS[this].test(CreditCard.strip(number));
 	}
 };
 
 (function(){
-  for(var card in CreditCard.CARDS) {
+	for(var card in CreditCard.CARDS) {
 		CreditCard["is"+card] = CreditCard.bindType(card);
 	};
 })();
